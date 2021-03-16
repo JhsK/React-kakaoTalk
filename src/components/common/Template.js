e0566c2088048bddfa87c4a6276e24debc25ef17
JhsK <qwe6293@nate.com>
2021-03-16 16:12:38 +0900

    [Feat] Template.js 파일 생성

---
+++ src/components/common/Template.js
@@ -0,0 +1,9 @@
+import React from "react";
+import palette from "../../lib/styles/palette";
+import styled from "styled-components";
+
+const Template = () => {
+  return <div></div>;
+};
+
+export default Template;
