9ed97bfdeec0246e9bae2e71518519883ccede29
JhsK <qwe6293@nate.com>
2021-03-18 15:15:58 +0900

    [Feat] FriendList 전용 styledComponent 작성 및 hover 적용

--- src/components/main/FriendList.js
+++ src/components/main/FriendList.js
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import { IoIosArrowUp } from "react-icons/io";
+import imgProfile from "../../image/profile.jpg";
+import palette from "../../lib/styles/palette";
 
 const friendsName = [
   "가영",
@@ -50,6 +52,57 @@ const FriendHeader = styled.div`
   padding: 0 1.3rem;
   opacity: 0.6;
   margin-bottom: 1.2rem;
+`;
+
+const StyledMyProfile = styled.div`
+  .profile-section {
+    display: flex;
+    align-items: center;
+    flex-basis: 60%;
+
+    .profile-img {
+      border-radius: 20px;
+      width: 50px;
+      height: 50px;
+      margin: 0;
+      outline: none;
+      background-image: url(${imgProfile});
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: center;
+      cursor: pointer;
+    }
+    .profile-name {
+      font-weight: bold;
+      font-size: 0.9rem;
+      margin-left: 0.7rem;
+    }
+  }
+  .profile-music {
+    border: 1px solid green;
+    border-radius: 15px;
+    font-size: 0.7rem;
+    padding: 0.3rem 0.6rem;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    flex-basis: 30%;
+
+    .music-icon {
+      font-size: 0.8rem;
+      margin-left: 0.2rem;
+    }
+  }
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  padding: 0.4rem 1.3rem;
+  margin-bottom: 0.5rem;
+  &:hover {
+    background: ${palette.gray[0]};
+  }
 `;
 
 const FriendList = () => {
