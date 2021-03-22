cad7cc97058ebf1a1c765a4344032b3feb0df921
JhsK <qwe6293@nate.com>
2021-03-22 16:34:31 +0900

    [Feat] 로그인, 회원가입에 사용할 공용 styled-component

---
+++ src/lib/styles/AuthBg.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const AuthBg = styled.div`
+  position: relative;
+  width: 350px;
+  height: 700px;
+  background: #fff;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 3rem;
+`;
+
+export default AuthBg;
---
+++ src/lib/styles/AuthBtn.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+const AuthBtn = styled.button`
+  background: #a8dfff;
+  border-radius: 20px;
+  width: 100%;
+  height: 40px;
+  color: #183152;
+  outline: none;
+  border: none;
+  cursor: pointer;
+
+  &:hover {
+    background: #abc8e2;
+  }
+`;
+
+export default AuthBtn;
