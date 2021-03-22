77619b94b05d6907b09eaac72628e564a1b5ed20
JhsK <qwe6293@nate.com>
2021-03-22 22:09:03 +0900

    [Feat] morgan 재설치 및 back.js 작성

---
+++ back.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const path = require("path");
+const session = require("express-session");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const pageRouter = require("./routes/page");
+
+const back = express();
+back.set("port", process.env.PORT || 8001);
+back.set("view engine", "html");
+
+back.use(morgan("dev"));
+back.use(express.static(path.join));
+back.use(express.json());
+back.use(express.urlencoded({ extended: false }));
+back.use(cookieParser(process.env.COOKIE_SECRET));
+back.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+  })
+);
+
+back.use("/", pageRouter);
+
+back.use((req, res, next) => {
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+back.use((err, req, res, next) => {
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+back.listen(back.get("port"), () => {
+  console.log(back.get("port"), "번 포트에서 대기중");
+});
